1758469d73da6e79b6d0d9e01a728dc867f88224
didi <didi@mobilogics.com.tw>
2015-03-26 23:24:26 +0800

    Import Groovy and using Groovy with Android

---
+++ app/src/main/groovy/tw/com/s11113153/wormholetraveller/MainActivity.groovy
@@ -0,0 +1,13 @@
+package tw.com.s11113153.wormholetraveller
+
+import android.app.Activity
+import android.os.Bundle
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
