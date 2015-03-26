8288e2f0f033fb0281be9e2bd75210e7dd98d265
didi <didi@mobilogics.com.tw>
2015-03-27 02:46:22 +0800

    finish loading and jump MainActivity

--- app/src/main/groovy/tw/com/s11113153/wormholetraveller/LoadingActivity.groovy
+++ app/src/main/groovy/tw/com/s11113153/wormholetraveller/LoadingActivity.groovy
@@ -5,28 +5,57 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import android.widget.Toast
+import android.util.TimeUtils
+import com.pnikosis.materialishprogress.ProgressWheel
+import groovy.transform.CompileStatic
 
 /**
  * Created by xuyouren on 15/3/26.
  */
+
+@CompileStatic
 public class LoadingActivity extends Activity {
   private static final String TAG = LoadingActivity.class.getSimpleName();
-  static final int DURATION = 1
+
+  static final float DURATION_TIME = 2.5f /** 2.5 sec **/
+  static final float SPEED_TIME = 1.0f / DURATION_TIME as float
+
+  private ProgressWheel mProgressWheel
+
+  private static boolean isLaunch = false
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState)
-    new Handler().postDelayed([
-      run : {
-            Intent intent = new Intent()
-            intent.setClass(this, MainActivity.class)
-            intent.addFlags(
-              Intent.FLAG_ACTIVITY_CLEAR_TASK |
-              Intent.FLAG_ACTIVITY_NEW_TASK
-            )
-            startActivity(intent)
+    setContentView(R.layout.activity_loading)
+
+    mProgressWheel = findViewById(R.id.progress_wheel) as ProgressWheel
+    mProgressWheel.setProgress(1.0f)
+    mProgressWheel.setSpinSpeed(SPEED_TIME)
+    mProgressWheel.setCallback([
+      onProgressUpdate : { float val ->
+        if (val == 1.0f && !isLaunch) {
+          toMainActivity()
+          isLaunch = true
+        } else {
+          Log.v(TAG, "" + String.valueOf(val));
+        }
       }
-    ] as Runnable, DURATION)
+    ] as ProgressWheel.ProgressCallback)
+  }
+
+  void toMainActivity() {
+    new Handler().post([
+      run : {
+        Intent intent = new Intent()
+            .setClass(this, MainActivity.class)
+            .addFlags(
+                Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                Intent.FLAG_ACTIVITY_NEW_TASK
+            )
+        startActivity(intent)
+        Log.d(TAG, "" + String.valueOf("Run"));
+      }
+    ] as Runnable)
   }
 }
--- app/src/main/groovy/tw/com/s11113153/wormholetraveller/MainActivity.groovy
+++ app/src/main/groovy/tw/com/s11113153/wormholetraveller/MainActivity.groovy
@@ -10,4 +10,9 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
+
+  @Override
+  void onBackPressed() {
+    moveTaskToBack(true)
+  }
 }
